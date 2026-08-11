f7081a285409f4ca83e15202a5847187a8dc596a
taygete1982-art <taygete1982@gmail.com>
2026-08-11 18:32:55 +0300

    рокапе

---
+++ scripts/game/main.gd
@@ -0,0 +1,17 @@
+﻿extends Node2D
+
+var Table = preload("res://scripts/game/table.gd")
+
+var table_instance
+
+func _ready():
+    table_instance = Table.new()
+    table_instance.build(self)
+
+func _physics_process(delta):
+    if table_instance:
+        table_instance.tick(delta)
+
+func _unhandled_input(event):
+    if table_instance:
+        table_instance.input(event)
